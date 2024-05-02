7862f7ab2cfecd8e52c747f477c80b2b958191d0
Paiv3nic <161754183+Paiv3nic@users.noreply.github.com>
2024-05-02 16:29:26 -0300

    js

--- js/contato.js
+++ js/contato.js
@@ -8,11 +8,25 @@ const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
 const campoEstado = formulario.querySelector("#estado");
 const botaoBuscar = formulario.querySelector("#buscar");
-const mensagem = formulario.querySelector("#status");
+const mensagemStatus = formulario.querySelector("#status");
 
 /* Detectando quando o botão de buscar CEP é acionado */
 botaoBuscar.addEventListener("click", function(event){
     /* Anular o comportamento da página. Sempre acontece ao trabalhar com <a> e <form>. */
     event.preventDefault();
+
     
+
+    /* Verificando se o cep NÃO TEM 8 dígitos */
+    if ( campoCep.value.length !== 8 ){
+        mensagemStatus.textContent = "Digite o CEP válido";
+        mensagemStatus.style.color = "purple"
+
+        /* Pare completamente a execução */
+        return;
+    }
+
+    /* Guardando o valor do cep digitado/informado */
+    let cepInformado = campoCep.value;
+    console.log(cepInformado);
 });
