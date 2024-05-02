9f178ea759d8ffdd9d9794fcc50bcf39cffeae89
Paiv3nic <161754183+Paiv3nic@users.noreply.github.com>
2024-05-02 15:28:23 -0300

    js

---
+++ js/contato.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/* Selecione os elementos que serão manipulados */
+const formulario = document.querySelector("form");
+const campoCep = formulario.querySelector("#cep");
+const campoEndereco = formulario.querySelector("#endereco");
+const campoBairro = formulario.querySelector("#bairro");
+const campoCidade = formulario.querySelector("#cidade");
+const campoEstado = formulario.querySelector("#estado");
+const botaoBuscar = formulario.querySelector("#buscar");
+const mensagem = formulario.querySelector("#status");
+
+/* Detectando quando o botão de buscar CEP é acionado */
+botaoBuscar.addEventListener("click", function(event){
+    /* Anular o comportamento da página. Sempre acontece ao trabalhar com <a> e <form>. */
+    event.preventDefault();
+    
+});
